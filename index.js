fbb17104e4686188b0b280fff9d8c5abf0417706
Candydev0 <105285244+Candydev0@users.noreply.github.com>
2022-05-14 22:10:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const {
   session,
   Markup
 } = require('telegraf');
-const url = "https://app-cacd4d01-cdd4-4d60-88cc-acabc38c3f9f.cleverapps.io/refer/";
+const url = "https://app-3d719d27-5f9d-4eb6-b8f8-9abd90562db8.cleverapps.io/refer/";
 const bot = new Telegraf("5138093222:AAGKD8jXFBbPdiU6ZnNQC6cHBPvJ5pdjld4");
 const setnew = (msg, refby, refst)=> {
   var sql = `INSERT INTO users(id, fname, lname, username, balance, refer_count, refer_by, total_earned, refer_status) VALUES (${msg.message.chat.id},'${msg.message.chat.first_name}','${msg.message.chat.last_name}','${msg.message.chat.username}',0,0,${refby},0,${refst})`;
